36d42432d931f10a3c0ad5d6dd77c48ce5446549
Ajithkumar10061998 <aajithkumar8055@gmail.com>
2024-09-11 19:49:18 +0530

    second project commit

--- src/test/java/org/gitter/Gitter.java
+++ src/test/java/org/gitter/Gitter.java
@@ -1,5 +1,13 @@
 package org.gitter;
 
 public class Gitter {
-
+	private void gitOne() {
+		System.out.println("ONE");
+	}	
+	private void gitTwo() {
+		System.out.println("TWO"); 
+	}
+	private void gitThree() {
+		System.out.println("THREE");
+	}
 }
